34890077986161ba29f96864a87be5a0bbdf1ed3
laitrungdung2000 <laidung29022000@gmail.com>
2021-10-03 21:29:11 +0700

    add search text area submit

--- src/main/java/com/example/dictionary/MainDictionaryController.java
+++ src/main/java/com/example/dictionary/MainDictionaryController.java
@@ -3,12 +3,17 @@ package com.example.dictionary;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import java.io.File;
+
+import javafx.scene.control.TextArea;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class MainDictionaryController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private TextArea searchText;
 
     @FXML
     protected void playWord() {
@@ -20,6 +25,12 @@ public class MainDictionaryController {
 
     @FXML
     protected void searchWord() {
-        welcomeText.setText("AAAA");
+        welcomeText.setText(searchText.getText());
     }
+
+    @FXML
+    protected void getSearchInput() {
+        searchText.getText();
+    }
+
 }
