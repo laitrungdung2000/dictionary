e399e9d303d597a0525c70a3f0ac92dd487c08a9
laitrungdung2000 <laidung29022000@gmail.com>
2021-10-07 23:47:07 +0700

    add dialog create word

--- src/main/java/com/example/dictionary/CreateDialog.java
+++ src/main/java/com/example/dictionary/CreateDialog.java
@@ -1,4 +1,22 @@
 package com.example.dictionary;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class CreateDialog {
+
+    @FXML
+    protected void close() {
+    }
+
+    @FXML
+    protected  void add(ActionEvent event) {
+        Node  source = (Node)  event.getSource();
+        Stage stage  = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
 }
--- src/main/java/com/example/dictionary/MainDictionaryController.java
+++ src/main/java/com/example/dictionary/MainDictionaryController.java
@@ -1,10 +1,15 @@
 package com.example.dictionary;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import java.io.File;
+import java.io.IOException;
 
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
@@ -31,6 +36,15 @@ public class MainDictionaryController {
     @FXML
     protected void getSearchInput() {
         searchText.getText();
+    }
+
+    @FXML
+    protected void openAddDialog() throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("create-word-dialog.fxml"));
+        Dialog dialog = new Dialog();
+        dialog.getDialogPane().setContent(root);
+        dialog.show();
     }
 
 }
