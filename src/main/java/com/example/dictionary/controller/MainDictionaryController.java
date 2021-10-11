bb84e7fa5319be5814065f377897f4b568eb740b
Do Van Dat <19020244@vnu.edu.vn>
2021-10-11 18:32:45 +0700

    update search

--- src/main/java/com/example/dictionary/controller/MainDictionaryController.java
+++ src/main/java/com/example/dictionary/controller/MainDictionaryController.java
@@ -5,14 +5,12 @@ import com.example.dictionary.model.Word;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -27,27 +25,23 @@ public class MainDictionaryController {
     private TextField searchText;
 
     @FXML
+    private ListView<String> listSearchWords;
+
+    @FXML
+    void onChangeInputText() {
+        listSearchWords.getItems().clear();
+        ArrayList<Word> words = Dictionary.dictionarySearcher(searchText.getText());
+        for (Word word: words) {
+            listSearchWords.getItems().add(word.getWordTarget());
+        }
+    }
+
+    @FXML
     protected void playWord() {
         String bip = "test.mp3";
         Media hit = new Media(new File(bip).toURI().toString());
         MediaPlayer mediaPlayer = new MediaPlayer(hit);
         mediaPlayer.play();
-    }
-
-    @FXML
-    protected void searchWord() {
-        String textSearch = searchText.getText();
-        ArrayList<Word> wordSearchResults = Dictionary.dictionarySearcher(textSearch);
-        System.out.print(wordSearchResults);
-        String list_results = "Các từ tìm được là: \n";
-        for(Word item: wordSearchResults) {
-            list_results = list_results + item.getWordExplain() + "   " + item.getWordTarget() + "\n";
-        }
-        results.setText(list_results);
-    }
-
-    @FXML
-    protected void getSearchInput() {
     }
 
     @FXML
