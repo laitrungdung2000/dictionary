372e92d58a1c117b9ec85615fe2e0a3b42aef11a
Do Van Dat <19020244@vnu.edu.vn>
2021-10-14 09:57:19 +0700

    update PlayWord

--- src/main/java/com/example/dictionary/controller/MainDictionaryController.java
+++ src/main/java/com/example/dictionary/controller/MainDictionaryController.java
@@ -43,7 +43,7 @@ public class MainDictionaryController {
     void showSelectedWord() {
         String targetWord = listSearchWords.getSelectionModel().getSelectedItem();
         Word resWord = Dictionary.dictionaryLookup(targetWord);
-        explainTextArea.setText(resWord.getWordTarget() + "\n" + resWord.getWordExplain());
+        explainTextArea.setText(resWord.getWordExplain());
     }
 
     @FXML
@@ -62,8 +62,9 @@ public class MainDictionaryController {
             voice.setRate(190);
             voice.setPitch(100);
             voice.setVolume(3);
-            String[] parts = explainTextArea.getText().split("\n");
-            voice.speak(parts[0]);
+//            String[] parts = explainTextArea.getText().split("\n");
+            String targetWord = listSearchWords.getSelectionModel().getSelectedItem();
+            voice.speak(targetWord);
         } catch (Exception e) {
             e.printStackTrace();
         }
